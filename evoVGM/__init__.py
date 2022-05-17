54371de3799b8d7aca2f643ca175793650df86b4
Amine M. Remita <remita.amine@courrier.uqam.ca>
2022-05-17 11:25:54 -0400

    update report module

--- evoVGM/__init__.py
+++ evoVGM/__init__.py
@@ -8,6 +8,6 @@
 #################################
 
 __author__ = "amine remita"
-__date__ = "20220514"
-__version__ = "0.7.3"
+__date__ = "20220516"
+__version__ = "0.7.3.2"
 __all__ = ["data", "models", "simulations", "utils", "reports"]
--- evoVGM/reports.py
+++ evoVGM/reports.py
@@ -21,7 +21,7 @@ def plt_elbo_ll_kl_rep_figure(
         plot_validation=False):
 
     fig_format= "png"
-    fig_dpi = 150
+    fig_dpi = 300
 
     fig_file = out_file+"."+fig_format
 
@@ -38,63 +38,74 @@ def plt_elbo_ll_kl_rep_figure(
 
     ax.set_rasterization_zorder(0)
  
-    elbo_color = "tomato"
-    ll_color = "teal"
-    kl_color = "black"
+    elbo_color = "#E9002D" #sharop red
+    #ll_color = "teal"
+    #kl_color = "black"
+    #elbo_color = "#FF1F5B"
+    ll_color =   "#226E9C"  # darker blue
+    kl_color =   "#7C1D69"  # pink
 
+    elbo_color_v = "tomato"
+    ll_color_v =   "#009ADE" # light blue
+    kl_color_v =   "#AF58BA"  # light pink
+    
     # plot means
     ax.plot(x, scores[:,0,:].mean(0), "-", color=elbo_color, 
-            label="Elbo") # ELBO train
-    ax2.plot(x, scores[:,2,:].mean(0), "-", color=kl_color,
-            label="KL") # KL train
+            label="ELBO", zorder=6) # ELBO train
     ax.plot(x, scores[:,1,:].mean(0), "-", color=ll_color,
-            label="Ll") # LL train
+            label="LogL", zorder=4) # LL train
+    ax2.plot(x, scores[:,2,:].mean(0), "-", color=kl_color,
+            label="KL_qp", zorder=4) # KL train
 
-    # plot stds
+    # plot stds 
     ax.fill_between(x,
             scores[:,0,:].mean(0)-scores[:,0,:].std(0), 
             scores[:,0,:].mean(0)+scores[:,0,:].std(0), 
             color=elbo_color,
-            alpha=0.2, zorder=-1, interpolate=True)
+            alpha=0.2, zorder=5, interpolate=True)
 
     ax.fill_between(x,
             scores[:,1,:].mean(0)-scores[:,1,:].std(0), 
             scores[:,1,:].mean(0)+scores[:,1,:].std(0),
             color=ll_color,
-            alpha=0.2, zorder=-2, interpolate=True)
+            alpha=0.2, zorder=3, interpolate=True)
 
     ax2.fill_between(x,
             scores[:,2,:].mean(0)-scores[:,2,:].std(0), 
             scores[:,2,:].mean(0)+scores[:,2,:].std(0), 
             color=kl_color,
-            alpha=0.2, zorder=-3, interpolate=True)
+            alpha=0.2, zorder=-6, interpolate=True)
 
     # plot validation
     if plot_validation:
-        ax.plot(x, scores[:,3,:].mean(0), "-.", color=elbo_color,
-                label="Elbo_val") # ELBO train
-        ax.plot(x, scores[:,4,:].mean(0), "-.", color=ll_color,
-                label="Ll_val") # LL train
-        ax2.plot(x, scores[:,5,:].mean(0), "-.", color=kl_color,
-                label="KL_val") # KL train
+        ax.plot(x, scores[:,3,:].mean(0), "-.", color=elbo_color_v,
+                label="ELBO_val", zorder=2) # ELBO val
+        ax.plot(x, scores[:,4,:].mean(0), "-.", color=ll_color_v,
+                label="LogL_val", zorder=0) # LL val
+        ax2.plot(x, scores[:,5,:].mean(0), "-.", color=kl_color_v,
+                label="KL_qp_val", zorder=2) # KL val
         
         ax.fill_between(x,
                 scores[:,3,:].mean(0)-scores[:,3,:].std(0), 
                 scores[:,3,:].mean(0)+scores[:,3,:].std(0), 
-                color=elbo_color,
-                alpha=0.1, zorder=-1, interpolate=True)
+                color=elbo_color_v,
+                alpha=0.1, zorder=1, interpolate=True)
 
         ax.fill_between(x,
                 scores[:,4,:].mean(0)-scores[:,4,:].std(0), 
                 scores[:,4,:].mean(0)+scores[:,4,:].std(0), 
-                color=ll_color,
-                alpha=0.1, zorder=-2, interpolate=True)
+                color=ll_color_v,
+                alpha=0.1, zorder=2, interpolate=True)
 
         ax2.fill_between(x,
                 scores[:,5,:].mean(0)-scores[:,5,:].std(0), 
                 scores[:,5,:].mean(0)+scores[:,5,:].std(0), 
-                color= kl_color,
-                alpha=0.1, zorder=-3, interpolate=True)
+                color= kl_color_v,
+                alpha=0.1, zorder=1, interpolate=True)
+
+    ax.set_zorder(ax2.get_zorder()+1)
+    ax.set_frame_on(False)
+
     #ax.set_ylim(y_limits)
     ax.set_ylim([None, 0])
     ax.set_xticks([t for t in range(1, nb_iters+1) if t==1 or\
@@ -112,6 +123,8 @@ def plt_elbo_ll_kl_rep_figure(
             loc='upper left', borderaxespad=0.)
     plt.suptitle(title)
 
+
     plt.savefig(fig_file, bbox_inches="tight", 
             format=fig_format, dpi=fig_dpi)
 
+    plt.close(f)
